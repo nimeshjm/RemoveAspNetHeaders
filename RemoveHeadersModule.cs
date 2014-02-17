9a529c518bb408ad151bdc5699297517c184e585
Nimesh Manmohanlal <nimeshjm@gmail.com>
2014-02-17 15:16:37 +0000

    Initial commit

---
+++ RemoveHeadersModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+
+namespace RemoveHeaders
+{
+    public class RemoveHeadersModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.PreSendRequestHeaders += OnContextOnPreSendRequestHeaders;
+        }
+
+        private void OnContextOnPreSendRequestHeaders(object sender, EventArgs args)
+        {
+            // Remove the "Server" HTTP Header from response
+            var app = sender as HttpApplication;
+            if (app == null || app.Context == null)
+            {
+                return;
+            }
+
+            var headers = app.Context.Response.Headers;
+            headers.Remove("Server");
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
